8a892c9d167e6fc9eeb045fc38a8cc120b670dfa
Ori <oriangel62@gmail.com>
2022-05-18 13:30:47 +0300

    ediit hard coded db

--- Repository/DB.cs
+++ Repository/DB.cs
@@ -3,12 +3,11 @@ namespace Repository
 {
     public class DB
     {
-        public List<Contact> listContacts { get; set; }
+        public List<User> ConnectedUser { get; set; }
         public DB()
         {
-            listContacts= new List<Contact>() { new Contact() { Id = 1, Last = "abc", LastDate = DateTime.Now, MessageList = null, NickName = "yes", Server = "abc" },
-                        new Contact() { Id = 2, Last = "abcd", LastDate = DateTime.Now, MessageList = null, NickName = "yes", Server = "abc" }
-                        };
+            ConnectedUser = new List<User>();
+            ConnectedUser.Add();
 
         }
 
---
+++ Repository/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+
+namespace Repository
+{
+    public class User
+    {
+        public List<Contact> listContacts { get; set; }
+
+        public User()
+        {
+            listContacts = new List<Contact>() { new Contact() { Id = 1, Last = "abc", LastDate = DateTime.Now, MessageList = null, NickName = "yes", Server = "abc" },
+                        new Contact() { Id = 2, Last = "abcd", LastDate = DateTime.Now, MessageList = null, NickName = "yes", Server = "abc" }
+                        };
+        }
+    }
+}
