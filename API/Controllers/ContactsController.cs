70035a5aa2b7c6cc5aba910c88e689295e357f16
Ori <oriangel62@gmail.com>
2022-05-27 14:48:07 +0300

    changes in comment

--- API/Controllers/ContactsController.cs
+++ API/Controllers/ContactsController.cs
@@ -8,7 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using Domain;
 using Services;
 //using API.Data;
-//using API.Migrations;
+using API.Migrations;
 
 
 namespace API.Controllers
@@ -17,186 +17,186 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ContactsController : Controller
     {
-        ////{
-        //private readonly PomeloDB _context;
-        //private IService contactService;
-        //private string userLogIn = "string";
-        //public ContactsController(PomeloDB context)
         //{
-        //    _context = context;
-        //    contactService = new Service(context);
-        //}
-        //[HttpGet]
-        //public async Task<ActionResult> Index()
-        //{
-        //    if (_context.Contact != null)
-        //    {
-        //        //var json = Newtonsoft.Json.JsonConvert.SerializeObject(contactService.GetContacts(userLogIn));
-        //        //return Json(await _context.Contact.Where(item => item.UserName == userLogIn).ToListAsync());
-        //        List<Contact> c =await contactService.GetContacts(userLogIn);
-        //        return Json(c);
+        private readonly PomeloDB _context;
+        private Service contactService;
+        private string userLogIn = "string";
+        public ContactsController(PomeloDB context)
+        {
+            _context = context;
+            contactService = new Service(context);
+        }
+        [HttpGet]
+        public async Task<ActionResult> Index()
+        {
+            if (_context.Contact != null)
+            {
+                //var json = Newtonsoft.Json.JsonConvert.SerializeObject(contactService.GetContacts(userLogIn));
+                //return Json(await _context.Contact.Where(item => item.UserName == userLogIn).ToListAsync());
+                List<Contact> c = await contactService.GetContacts(userLogIn);
+                return Json(c);
 
-        //    }
-        //    return BadRequest();
+            }
+            return BadRequest();
 
-        //    //                  View(await _context.Contact.ToListAsync()) :
-        //    //                  Problem("Entity set 'PomeloDB.Contact'  is null.");
-        //    //return Json(contactService.GetContacts(userLogIn));
+            //                  View(await _context.Contact.ToListAsync()) :
+            //                  Problem("Entity set 'PomeloDB.Contact'  is null.");
+            //return Json(contactService.GetContacts(userLogIn));
 
-        //}
-
-
-        //// GET: ContactsController/Details/5
-
-        //[HttpGet("[action]/{id}")]
-        //public async Task<ActionResult> Details(string id)
-        //{
-        //    Contact c = await _context.Contact.FirstOrDefaultAsync(item => (item.Id == id) && item.UserName == userLogIn);
-        //    return Json(c);
-        //    //return Json(contactService.GetContact(userLogIn, id));
-        //}
-
-        //[HttpGet("{id}/[action]")]
-        //public async Task<ActionResult> Messages(string id)
-        //{
-
-        //    //if (_context.Message == null)
-        //    //{
-        //    //    return null;
-        //    //}
-
-        //    //List<Message> m = await _context.Message.Where(item => (item.From == userLogIn) && (item.To == id)).ToListAsync();
-
-        //    //if (m == null)
-        //    //{
-        //    //    return null;
-        //    //}
-
-        //    List<Message> m = await _context.Message.Where(item => (item.From == userLogIn) && (item.To == id)).ToListAsync();
+        }
 
 
-        //    //return Json(contactService.GetMessages(userLogIn, id));
-        //    return Json(m);
-        //}
+        // GET: ContactsController/Details/5
+
+        [HttpGet("[action]/{id}")]
+        public async Task<ActionResult> Details(string id)
+        {
+            Contact c = await _context.Contact.FirstOrDefaultAsync(item => (item.ContactName == id) && item.UserName == userLogIn);
+            return Json(c);
+            //return Json(contactService.GetContact(userLogIn, id));
+        }
+
+        [HttpGet("{id}/[action]")]
+        public async Task<ActionResult> Messages(string id)
+        {
+
+            //if (_context.Message == null)
+            //{
+            //    return null;
+            //}
+
+            //List<Message> m = await _context.Message.Where(item => (item.From == userLogIn) && (item.To == id)).ToListAsync();
+
+            //if (m == null)
+            //{
+            //    return null;
+            //}
+
+            List<Message> m = await _context.Message.Where(item => (item.From == userLogIn) && (item.To == id)).ToListAsync();
+
+
+            //return Json(contactService.GetMessages(userLogIn, id));
+            return Json(m);
+        }
 
 
 
-        //[HttpGet("{id}/messages/{id2}")]
-        //public async Task<ActionResult> IdMessage(int id2)
-        //{
-        //    return Json(await _context.Message.FirstOrDefaultAsync(item => item.Id == id2));
+        [HttpGet("{id}/messages/{id2}")]
+        public async Task<ActionResult> IdMessage(int id2)
+        {
+            return Json(await _context.Message.FirstOrDefaultAsync(item => item.Id == id2));
 
-        //    //return Json(contactService.GetMessage(id2));
-        //}
-
-
-        //[HttpPost]
-        //public async Task<IActionResult> CreateContact([Bind("Id,NickName,Server")] Contact contact)
-        //{
-        //    if (ModelState.IsValid)
-        //    { 
-        //        contactService.AddContact(contact);
-        //        return Ok();
-        //}
-        //    return  BadRequest();
-        //}
-
-        //[HttpPut("[action]/{id}")]
-        //public async Task<IActionResult> EditContact(string id, [Bind("NickName,Server")] Contact contact)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        contactService.UpdateContact(contact);
-        //        return Ok();
-        //    }
-        //    return BadRequest();
-        //}
+            //return Json(contactService.GetMessage(id2));
+        }
 
 
-        //[HttpDelete("[action]/{id}")]
-        //public async Task<IActionResult> DeleteContact(string id)
-        //{
+        [HttpPost]
+        public async Task<IActionResult> CreateContact([Bind("Id,NickName,Server")] Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                contactService.AddContact(contact);
+                return Ok();
+            }
+            return BadRequest();
+        }
 
-        //    if (ModelState.IsValid)
-        //    {
-        //        contactService.DeleteContact(userLogIn, id);
-        //        return Ok();
-        //    }
-        //    return BadRequest();
-        //}
-
-
-        //[HttpGet("[action]/{id}/messages")]
-        //public async Task<ActionResult> GetMeessages(string idContct)
-        //{
-        //    return Json(contactService.GetMessages(userLogIn, idContct));
-        //}
-
-
-        //[HttpGet("[action]/{id}/messages/{id2}")]
-        //public async Task<ActionResult> GetMeessage(string id,int id2)
-        //{
-        //    return Json(contactService.GetMessage(id2));
-        //}
+        [HttpPut("[action]/{id}")]
+        public async Task<IActionResult> EditContact(string id, [Bind("NickName,Server")] Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                contactService.UpdateContact(contact);
+                return Ok();
+            }
+            return BadRequest();
+        }
 
 
-        //[HttpPost("[action]/{id}/messages")]
-        //public async Task<IActionResult> CreateMessage(string id,[Bind("Type, Content , Sent, Created")] Message message)
-        //{
-        //    //add from to (sent) id and login name 
-        //    if (ModelState.IsValid)
-        //    {
-        //        message.From = userLogIn;
-        //        message.To = id;
-        //        //message.Created = DateTime.Now;
-        //        Message m2 = new Message()
-        //        {
-        //            Type = message.Type,
-        //            Content = message.Content,
-        //            Sent = !(message.Sent),
-        //            Created = message.Created,
-        //            From = id,
-        //            To = userLogIn
-        //        };
-        //        contactService.AddMessage(message);
-        //        contactService.AddMessage(m2);
-        //        return Ok();
-        //    }
-        //    return BadRequest();
-        //}
+        [HttpDelete("[action]/{id}")]
+        public async Task<IActionResult> DeleteContact(string id)
+        {
+
+            if (ModelState.IsValid)
+            {
+                contactService.DeleteContact(userLogIn, id);
+                return Ok();
+            }
+            return BadRequest();
+        }
 
 
-        //[HttpPut("[action]/{id}/messages/{id2}")]
-        //public async Task<IActionResult> EditMessage(int id2,[Bind("Id, Type, Content , Sent")] Message message)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        contactService.UpdateMessage(message);
-        //        return Ok();
-        //    }
-        //    return BadRequest();
-        //}
+        [HttpGet("[action]/{id}/messages")]
+        public async Task<ActionResult> GetMeessages(string idContct)
+        {
+            return Json(contactService.GetMessages(userLogIn, idContct));
+        }
 
 
-        //[HttpDelete("[action]/{id}/messages/{id2}")]
-        //public async Task<IActionResult> DeleteMessage(int id2)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        contactService.DeleteMessage(id2);
-        //        return Ok();
-        //    }
-        //    return BadRequest();
-        //}
+        [HttpGet("[action]/{id}/messages/{id2}")]
+        public async Task<ActionResult> GetMeessage(string id, int id2)
+        {
+            return Json(contactService.GetMessage(id2));
+        }
 
 
-        //[HttpGet("[action]/time")]
-        //public async Task<ActionResult> GetTime()
-        //{
-        //    return Json(DateTime.Now);
-        //}
+        [HttpPost("[action]/{id}/messages")]
+        public async Task<IActionResult> CreateMessage(string id, [Bind("Type, Content , Sent, Created")] Message message)
+        {
+            //add from to (sent) id and login name 
+            if (ModelState.IsValid)
+            {
+                message.From = userLogIn;
+                message.To = id;
+                //message.Created = DateTime.Now;
+                Message m2 = new Message()
+                {
+                    Type = message.Type,
+                    Content = message.Content,
+                    Sent = !(message.Sent),
+                    Created = message.Created,
+                    From = id,
+                    To = userLogIn
+                };
+                contactService.AddMessage(message);
+                contactService.AddMessage(m2);
+                return Ok();
+            }
+            return BadRequest();
+        }
 
-       
+
+        [HttpPut("[action]/{id}/messages/{id2}")]
+        public async Task<IActionResult> EditMessage(int id2, [Bind("Id, Type, Content , Sent")] Message message)
+        {
+            if (ModelState.IsValid)
+            {
+                contactService.UpdateMessage(message);
+                return Ok();
+            }
+            return BadRequest();
+        }
+
+
+        [HttpDelete("[action]/{id}/messages/{id2}")]
+        public async Task<IActionResult> DeleteMessage(int id2)
+        {
+            if (ModelState.IsValid)
+            {
+                contactService.DeleteMessage(id2);
+                return Ok();
+            }
+            return BadRequest();
+        }
+
+
+        [HttpGet("[action]/time")]
+        public async Task<ActionResult> GetTime()
+        {
+            return Json(DateTime.Now);
+        }
+
+
 
 
         //// GET: Contacts1/Create
@@ -220,93 +220,93 @@ namespace API.Controllers
         //    return await Json(contact);
         //}
 
-        //    // GET: Contacts1/Edit/5
-        //    public async Task<IActionResult> Edit(string id)
+        //// GET: Contacts1/Edit/5
+        //public async Task<IActionResult> Edit(string id)
+        //{
+        //    if (id == null || _context.Contact == null)
         //    {
-        //        if (id == null || _context.Contact == null)
-        //        {
-        //            return NotFound();
-        //        }
-
-        //        var contact = await _context.Contact.FindAsync(id);
-        //        if (contact == null)
-        //        {
-        //            return NotFound();
-        //        }
-        //        return View(contact);
+        //        return NotFound();
         //    }
 
-        //    // POST: Contacts1/Edit/5
-        //    // To protect from overposting attacks, enable the specific properties you want to bind to.
-        //    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        //    [HttpPost]
-        //    [ValidateAntiForgeryToken]
-        //    public async Task<IActionResult> Edit(string id, [Bind("Id,NickName,Server,Last,LastDate")] Contact contact)
+        //    var contact = await _context.Contact.FindAsync(id);
+        //    if (contact == null)
         //    {
-        //        if (id != contact.Id)
-        //        {
-        //            return NotFound();
-        //        }
+        //        return NotFound();
+        //    }
+        //    return View(contact);
+        //}
 
-        //        if (ModelState.IsValid)
+        //// POST: Contacts1/Edit/5
+        //// To protect from overposting attacks, enable the specific properties you want to bind to.
+        //// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Edit(string id, [Bind("Id,NickName,Server,Last,LastDate")] Contact contact)
+        //{
+        //    if (id != contact.Id)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    if (ModelState.IsValid)
+        //    {
+        //        try
         //        {
-        //            try
+        //            _context.Update(contact);
+        //            await _context.SaveChangesAsync();
+        //        }
+        //        catch (DbUpdateConcurrencyException)
+        //        {
+        //            if (!ContactExists(contact.Id))
         //            {
-        //                _context.Update(contact);
-        //                await _context.SaveChangesAsync();
+        //                return NotFound();
         //            }
-        //            catch (DbUpdateConcurrencyException)
+        //            else
         //            {
-        //                if (!ContactExists(contact.Id))
-        //                {
-        //                    return NotFound();
-        //                }
-        //                else
-        //                {
-        //                    throw;
-        //                }
+        //                throw;
         //            }
-        //            return RedirectToAction(nameof(Index));
         //        }
-        //        return View(contact);
-        //    }
-
-        //    // GET: Contacts1/Delete/5
-        //    public async Task<IActionResult> Delete(string id)
-        //    {
-        //        if (id == null || _context.Contact == null)
-        //        {
-        //            return NotFound();
-        //        }
-
-        //        var contact = await _context.Contact
-        //            .FirstOrDefaultAsync(m => m.Id == id);
-        //        if (contact == null)
-        //        {
-        //            return NotFound();
-        //        }
-
-        //        return View(contact);
-        //    }
-
-        //    // POST: Contacts1/Delete/5
-        //    [HttpPost, ActionName("Delete")]
-        //    [ValidateAntiForgeryToken]
-        //    public async Task<IActionResult> DeleteConfirmed(string id)
-        //    {
-        //        if (_context.Contact == null)
-        //        {
-        //            return Problem("Entity set 'PomeloDB.Contact'  is null.");
-        //        }
-        //        var contact = await _context.Contact.FindAsync(id);
-        //        if (contact != null)
-        //        {
-        //            _context.Contact.Remove(contact);
-        //        }
-
-        //        await _context.SaveChangesAsync();
         //        return RedirectToAction(nameof(Index));
         //    }
+        //    return View(contact);
+        //}
+
+        //// GET: Contacts1/Delete/5
+        //public async Task<IActionResult> Delete(string id)
+        //{
+        //    if (id == null || _context.Contact == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    var contact = await _context.Contact
+        //        .FirstOrDefaultAsync(m => m.Id == id);
+        //    if (contact == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return View(contact);
+        //}
+
+        //// POST: Contacts1/Delete/5
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> DeleteConfirmed(string id)
+        //{
+        //    if (_context.Contact == null)
+        //    {
+        //        return Problem("Entity set 'PomeloDB.Contact'  is null.");
+        //    }
+        //    var contact = await _context.Contact.FindAsync(id);
+        //    if (contact != null)
+        //    {
+        //        _context.Contact.Remove(contact);
+        //    }
+
+        //    await _context.SaveChangesAsync();
+        //    return RedirectToAction(nameof(Index));
+        //}
 
         //private bool ContactExists(string id)
         //{
--- API/Services/Service.cs
+++ API/Services/Service.cs
@@ -9,139 +9,139 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Services
 {
-    public class Service : IService
+    public class Service 
     {
-        //    private readonly PomeloDB _context;
+        private readonly PomeloDB _context;
 
-        //    public Service(PomeloDB _context1)
-        //    {
-        //        _context = _context1;
+        public Service(PomeloDB _context1)
+        {
+            _context = _context1;
 
-        //    }
+        }
 
-        //    public async Task<Contact> GetContact(string userId, string id)
-        //    {
-        //        //Contact c = await _context.Contact.FirstOrDefaultAsync(item => (item.Id == id) && item.UserName == userId);
-        //        List<Contact> c = await _context.Contact.Where(item => (item.Id == id) && item.UserName == userId).ToListAsync();
-        //        if (c == null)
-        //        {
-        //            return null;
-        //        }
+        public async Task<Contact> GetContact(string userId, string id)
+        {
+            //Contact c = await _context.Contact.FirstOrDefaultAsync(item => (item.Id == id) && item.UserName == userId);
+            List<Contact> c = await _context.Contact.Where(item => (item.ContactName == id) && item.UserName == userId).ToListAsync();
+            if (c == null)
+            {
+                return null;
+            }
 
-        //        return c[0];
-        //    }
-        //    public async Task<List<Contact>> GetContacts(string user)
-        //    {
+            return c[0];
+        }
+        public async Task<List<Contact>> GetContacts(string user)
+        {
 
-        //        //List<Contact> c= await _context.Contact.Where(item => item.User.UserName == user).ToListAsync();
-        //        //if (c == null)
-        //        //{
-        //        //    return null;
-        //        //}
+            //List<Contact> c= await _context.Contact.Where(item => item.User.UserName == user).ToListAsync();
+            //if (c == null)
+            //{
+            //    return null;
+            //}
 
 
-        //        List< Contact > c = await _context.Contact.Where(item => item.UserName == user).ToListAsync();
-        //        return c;
+            List<Contact> c = await _context.Contact.Where(item => item.UserName == user).ToListAsync();
+            return c;
 
-        //    }
+        }
 
-        //    public async Task<List<Message>> GetMessages(string user, string contact)
-        //    {
-        //        if(_context.Message == null)
-        //        {
-        //            return null;
-        //        }
+        public async Task<List<Message>> GetMessages(string user, string contact)
+        {
+            if (_context.Message == null)
+            {
+                return null;
+            }
 
-        //        List<Message> m = await _context.Message.Where(item => (item.From == user) && (item.To == contact)).ToListAsync();
+            List<Message> m = await _context.Message.Where(item => (item.From == user) && (item.To == contact)).ToListAsync();
 
-        //        if (m == null)
-        //        {
-        //            return null;
-        //        }
+            if (m == null)
+            {
+                return null;
+            }
 
-        //        return m;
-        //    }
+            return m;
+        }
 
-        //    public async Task<Message> GetMessage(int id)
-        //    {
+        public async Task<Message> GetMessage(int id)
+        {
 
-        //        return await _context.Message.FirstOrDefaultAsync(item => item.Id == id);
-        //    }
+            return await _context.Message.FirstOrDefaultAsync(item => item.Id == id);
+        }
 
-        //    public async Task<bool?> AddContact(Contact contact)
-        //    {
-        //        if (_context.Contact == null || contact == null)
-        //        {
-        //            return null;
-        //        }
-        //        //using (var db = new PomeloDB())
-        //        //{
-        //        _context.Contact.Add(contact);
-        //        try
-        //        {
-        //            await _context.SaveChangesAsync();
-        //        }
-        //        catch (DbUpdateConcurrencyException)
-        //        {
-        //            throw;
-        //        }
-        //        return true;
-        //        //}
-        //        //db.Contact.Add(contact);
-        //        //db.SaveChanges();
-        //    }
-        //    public async void UpdateContact(Contact contact)
-        //    {
-        //        _context.Contact.Update(contact);
-        //        await _context.SaveChangesAsync();
+        public async Task<bool?> AddContact(Contact contact)
+        {
+            if (_context.Contact == null || contact == null)
+            {
+                return null;
+            }
+            //using (var db = new PomeloDB())
+            //{
+            _context.Contact.Add(contact);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                throw;
+            }
+            return true;
+            //}
+            //db.Contact.Add(contact);
+            //db.SaveChanges();
+        }
+        public async void UpdateContact(Contact contact)
+        {
+            _context.Contact.Update(contact);
+            await _context.SaveChangesAsync();
 
-        //    }
+        }
 
-        //    public async void DeleteContact(string userId,string contactId)
-        //    {
-        //        //async Contact c = GetContact(id);
-        //        List<Contact> x = await _context.Contact.Where(item => (item.UserName == userId) && (item.Id == contactId)).ToListAsync();
-        //        _context.Contact.Remove(x[0]);
-        //        await _context.SaveChangesAsync();
-        //    }
+        public async void DeleteContact(string userId, string contactId)
+        {
+            //async Contact c = GetContact(id);
+            List<Contact> x = await _context.Contact.Where(item => (item.UserName == userId) && (item.ContactName == contactId)).ToListAsync();
+            _context.Contact.Remove(x[0]);
+            await _context.SaveChangesAsync();
+        }
 
-        //    public async void AddMessage(Message message)
-        //    {
+        public async void AddMessage(Message message)
+        {
 
-        //        _context.Message.Add(message);
-        //        await _context.SaveChangesAsync();
-        //        //db.Contact.Add(contact);
-        //        //db.SaveChanges();
-        //    }
+            _context.Message.Add(message);
+            await _context.SaveChangesAsync();
+            //db.Contact.Add(contact);
+            //db.SaveChanges();
+        }
 
-        //    public async void UpdateMessage(Message message)
-        //    {
-        //        //maby need to find dirst the key
-        //        _context.Message.Update(message);
-        //        await _context.SaveChangesAsync();
-        //        //db.Contact.Add(contact);
-        //        //db.SaveChanges();
-        //    }
+        public async void UpdateMessage(Message message)
+        {
+            //maby need to find dirst the key
+            _context.Message.Update(message);
+            await _context.SaveChangesAsync();
+            //db.Contact.Add(contact);
+            //db.SaveChanges();
+        }
 
-        //    public async void DeleteMessage(int idMessage)
-        //    {
-        //        //async Contact c = GetContact(id);
-        //        Message x = await _context.Message.FindAsync(idMessage);
-        //        _context.Message.Remove(x);
-        //        await _context.SaveChangesAsync();
-        //    }
+        public async void DeleteMessage(int idMessage)
+        {
+            //async Contact c = GetContact(id);
+            Message x = await _context.Message.FindAsync(idMessage);
+            _context.Message.Remove(x);
+            await _context.SaveChangesAsync();
+        }
 
-        //}
-        ////public void DeleteContact(Contact contact)
-        ////{
-        ////    using (var db = new PomeloDB())
-        ////    {
-        ////        db.Contact.Remove(contact);
-        ////        db.SaveChanges();
-        ////    }
     }
-
+    //public void DeleteContact(Contact contact)
+    //{
+    //    using (var db = new PomeloDB())
+    //    {
+    //        db.Contact.Remove(contact);
+    //        db.SaveChanges();
+    //    }
 }
+
+
 
 
 
